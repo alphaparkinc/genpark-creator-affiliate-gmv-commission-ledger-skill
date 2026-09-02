ee1001b0cf7baff84958bdcb5136b4fd867596de
GenPark <135203240+alphaparkinc@users.noreply.github.com>
2026-09-02 18:23:49 +0800

    feat: add client.py

---
+++ client.py
@@ -0,0 +1,12 @@
+class CreatorAffiliateGmvCommissionLedgerClient:
+    def calculate_affiliate_payouts(self, creator_id='crt_991823', attribution_period='2026-08', base_commission_rate_pct=15.0):
+        return {
+            'payout_ledger_id': 'pay_ldg_5519',
+            'creator_id': creator_id,
+            'total_attributed_orders_count': 1420,
+            'gross_merchandise_value_gmv_usd': 85400.00,
+            'earned_commission_payout_usd': 12810.00,
+            'refund_clawback_adjusted_amount_usd': 340.00,
+            'net_disbursable_payout_usd': 12470.00,
+            'itemized_affiliate_statement_url': 'https://affiliates.commerce.genpark.ai/statements/5519.json'
+        }
